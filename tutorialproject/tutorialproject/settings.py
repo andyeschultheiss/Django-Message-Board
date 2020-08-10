83a4eaff1a180efc510f39591a58cb9a8380df76
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-08-10 16:45:14 -0700

    initial commit

--- tutorialproject/tutorialproject/settings.py
+++ tutorialproject/tutorialproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9n-=64cczwjpr2-=09pv^j0ob&@!sew9uv=l#s%ea3trl9z%)q'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
