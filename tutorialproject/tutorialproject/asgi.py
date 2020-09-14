df8fb32e56271cbd4cded4cdda22d302c4b643c2
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-09-14 14:17:07 -0700

    update wsgi.py with correct settings path

--- tutorialproject/tutorialproject/asgi.py
+++ tutorialproject/tutorialproject/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorialproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorialproject.tutorialproject.settings')
 
 application = get_asgi_application()
